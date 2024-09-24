54aed42f9e9dd57a0793a99b905900b2cbf74d0e
rugtumu <umutgur7@gmail.com>
2024-09-24 21:49:39 +0300

    Error inside Card.tsx is solved

--- components/Card.tsx
+++ components/Card.tsx
@@ -44,8 +44,7 @@ const Card = ({ title, description, imgSrc, href }) => (
             href={href}
             className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
-          >
-          </Link>
+          ></Link>
         )}
       </div>
     </div>
